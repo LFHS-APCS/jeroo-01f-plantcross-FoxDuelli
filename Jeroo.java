42fcae68dcd5d251366ccf1641f86bbce55c91a1
FoxDuelli <69988422+FoxDuelli@users.noreply.github.com>
2020-08-31 15:05:37 -0500

    Update Jeroo.java

--- Jeroo.java
+++ Jeroo.java
@@ -5,10 +5,65 @@
 public class Jeroo extends JerooBase {
 
     
-    public void plantCross() {
+    public void plantCross() { 
+  hopTwo();
+  hopTwo();
+  hopTwo();
+  turn(RIGHT);
+  hopTwo();
+  hopPlant();
+  hopPlantThree();
+  turn(RIGHT);
+  hopPlantThree();
+  turn(LEFT);
+  hopPlantTwo();
+  turn(LEFT);
+  hopPlantThree();
+  turn(RIGHT);
+  hopPlantThree();
+  turn(LEFT);
+  hopPlantTwo();
+  turn(LEFT);
+  hopPlantThree();
+  turn(RIGHT);
+  hopPlantThree();
+  turn(LEFT);
+  hopPlantTwo();
+  turn(LEFT);
+  hopPlantThree();
+  turn(RIGHT);
+  hopPlantThree();
+  turn(LEFT);
+  hopPlant();
+  turn(RIGHT);
+  hop();
+  }
         
     }
     // Put any helpermethods here
+    
+  
+// merges the commands hop and plant together 
+public void hopPlant() {
+  hop();
+  plant();
+}
+//uses the hopPlant command three times
+public void hopPlantThree() {
+  hopPlant();
+  hopPlantTwo();
+}
+//uses the hopPlant command two times
+public void hopPlantTwo() {
+  hopPlant();
+  hopPlant();
+}
+//makes the Jeroo hop twice
+public void hopTwo() {
+  hop();
+  hop();
+
+}
     
     
     
